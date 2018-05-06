58ab603856acad56ebbb18b5fa7b465dcd6ba87b
Matthías Páll Gissurarson <mpg@mpg.is>
2018-05-06 22:10:58 +0200

    readValue now works

--- InkyPhat.hs
+++ InkyPhat.hs
@@ -6,11 +6,12 @@ module InkyPhat (
     -- Data
     image, size, dimensions
     -- Types
-    , Color (..) , Font
+    , Color (..) , Font (..), Image
     ) where 
 
 
 import System.Process
+import Control.Concurrent
 import GHC.IO.Handle
 import Data.Maybe (fromMaybe)
 import Control.Monad (join)
@@ -25,21 +26,17 @@ type InkyIO = ReaderT (Handle, Handle) IO
 -- To avoid issues with FFI, we'll just go the easy
 -- interpeter way.
 sendCommand :: String -> InkyIO ()
-sendCommand cmd = lift $  print cmd
-    -- do (stdin, _) <- ask
-    --    lift $ do hPutStr stdin (cmd ++ "\n")
-    --    hFlush stdin
+sendCommand cmd =
+    do (stdin, _) <- ask
+       lift $ do hPutStr stdin (cmd ++ "\n")
+                 hFlushAll stdin
 
--- readValue :: Read a => String -> InkyIO a
-readValue cmd = lift (print cmd) >> return (0,0)
-  -- do (stdin, stdout) <- ask
-  --    lift $ hFlushAll stdout
-  --    sendCommand cmd
-  --    lift $ read <$> hGetLine stdout
-
--- readValue :: String -> InkyIO (Int,Int)
--- readValue cmd = lift $ print cmd >> return (0,0)
-
+readValue :: Read a => String -> InkyIO a
+readValue cmd =
+  do (stdin, stdout) <- ask
+     lift $ hFlushAll stdout
+     sendCommand cmd 
+     lift $ read <$> hGetLine stdout
 
 initialCommands :: [String]
 initialCommands
@@ -62,7 +59,7 @@ instance InkyPhatVal Font where
   toIPVal (Font a) = "inkyphat.ImageFont.truetype(inkyphat.fonts.PressStart2P," ++ show a ++")"
 
 display :: InkyIO ()
-display = sendCommand  "inkyphat.show()"
+display = sendCommand "inkyphat.show()"
 
 clear :: InkyIO ()
 clear = sendCommand  "inkyphat.clear()"
@@ -83,7 +80,7 @@ image name loc = do sendCommand $ join [ name
 
 paste :: Image -> (Int, Int) -> InkyIO ()
 paste img loc = 
-  sendCommand $ intercalate "," $ ["inkyphat.paste(" ++ show img
+  sendCommand $ intercalate "," $ ["inkyphat.paste(" ++ img
                                   , show loc ++ ")"]
 
 text :: (Int, Int) -> String -> Maybe Color -> Maybe Font -> InkyIO ()
@@ -96,17 +93,17 @@ text xy text color font =
         col =  fromMaybe Black color
 
 size :: Image -> InkyIO (Int, Int)
-size img = readValue $ join [img, ".size"]
+size img = readValue $ "(" ++ img ++ ".size[0], " ++ img ++ ".size[1])"
 
 dimensions :: InkyIO (Int, Int)
 dimensions = readValue "(inkyphat.WIDTH, inkyphat.HEIGHT)"
 
 runInky :: InkyIO a -> IO a 
 runInky action =
-  do (Just stdin, Just stdout , _, proc) <- createProcess $ cp { std_in = CreatePipe, std_out = CreatePipe }
+  do (Just stdin, Just stdout , _, proc) <- createProcess $ cp { std_in = CreatePipe, std_out = CreatePipe, std_err = CreatePipe }
      r <- flip runReaderT (stdin, stdout) $
-            do mapM_ sendCommand  initialCommands
+            do mapM_ sendCommand initialCommands
                action
      mapM_ hClose [stdin, stdout]
      return r
-  where cp = shell "python3"
+  where cp = shell "python3 -i"
--- Main.hs
+++ Main.hs
@@ -6,31 +6,23 @@ import InkyPhat
 
 import Data.Maybe
 import Data.Time
+import Data.List (intercalate)
+import Control.Monad.Reader (lift)
 
 löviksVägen :: BusStop
 löviksVägen = 9021014004663000
 hovåsNedre :: BusStop
 hovåsNedre = 9021014003235000
 
-setup :: InkyIO ()
-setup = do setRotation 180
-           vt <- image "vt" "vt.png"
-           return ()
+setup :: InkyIO Image
+setup = setRotation 180 >> image "vt" "vt.png"
 
 fontheight :: Int
 fontheight = 12
+fontwidth = fontheight
 linespace :: Int
 linespace = 2
 
-printBusTimes :: [String] -> InkyIO ()
-printBusTimes msgs =
-  do (inkyH, inkyW) <- dimensions
-     let (x,y) = (2, (inkyH `div` 2 - fontheight*(length msgs) `div` 2))
-         pr (m,i) = text pos m Nothing Nothing  
-          where pos = (x, y + i*(fontheight + linespace))
-     mapM_ pr $ zip msgs [0.. ]
-
-  where pos = fromMaybe
 
 displayNames :: BusLine -> String
 displayNames (BL "82" "A" _) = "82"
@@ -57,26 +49,66 @@ rightpad c i n = i ++ replicate (n - length i) c
 
 renderBusLine :: TimeOfDay -> BusLine -> String
 renderBusLine now bl@(BL _ _ dp) =
-   concatMap to5 $ (displayNames bl ++ ":"):dpts
+   unwords $ map to5 $ (displayNames bl ++ ":"):dpts
  where to5 s = leftpad ' ' s 5
        dpts = rightpad "-" (map toDp dp) 2
-       toDp t = lt - nowSecs 
-        where lt = timeOfDayToTime $ read @TimeOfDay t
-              nowSecs = timeOfDayToTime now
-      
+       toDp t = msg 
+         where mins = floor (lt - nowSecs) `div` 60
+               msg = if mins > 0 then (show mins) ++ "min" else "Núna!"
+               lt = timeOfDayToTime $ read @TimeOfDay (t ++ ":00")
+               nowSecs = timeOfDayToTime now
+
+printBusTimes :: [String] -> InkyIO ()
+printBusTimes msgs =
+  do (inkyW, inkyH) <- dimensions
+     let (x,y) = (2, (inkyH `div` 2 - fontheight*(length msgs) `div` 2))
+         pr (m,i) = text pos m Nothing Nothing  
+          where pos = (x, y + i*(fontheight + linespace))
+     mapM_ pr $ zip msgs [0.. ]
+  where pos = fromMaybe
+
+printTime :: InkyIO ()
+printTime = do (date, time) <- lift getDateTime
+               (inkyW, _) <- dimensions
+               let pos = ((inkyW - 16*fontwidth) `div` 2,18)
+               text pos (unwords [date, time]) (Just Red) Nothing
+
+printName :: InkyIO ()
+printName = do (inkyW, _) <- dimensions
+               let pos = ((inkyW - 14*fontwidth) `div` 2,2)
+               text pos "Strætóferðir" (Just Red) (Just (Font 14))
+
+printLogo :: Image -> InkyIO ()
+printLogo img
+ = do (inkyW, inkyH) <- dimensions
+      (logoW, logoH) <- size img
+      let (vx,vy) =  ((inkyW - 10*14 - logoW -10) `div` 2, inkyH -2-14)
+          posText = (vx + logoW+2, vy -4)
+          posLogo = (vx, vy-logoH+14-2)
+      text posText "Västtrafik" (Just Red) (Just (Font 14))
+      paste img posLogo
+  
+
+updateDisplay :: Image -> [String] -> InkyIO ()
+updateDisplay img times
+ = do { printName
+      ; printTime
+      ; printBusTimes times
+      ; printLogo img
+      ; display }
+
+
 main :: IO ()
 main = do 
     (date, time) <- getDateTime
-    runInky $ do setup
-                 printBusTimes ["82: - -", "ROSA: - -"]
     token <- access_token <$> getToken
     lvg <- getBusTimes löviksVägen token
     hvs <- getBusTimes hovåsNedre token
     print lvg
     print hvs
-    print $ map (renderBusLine $ read @TimeOfDay time) $ interestingLines $ getLines lvg <> getLines hvs
-
-    runInky $ do setup
-                 printBusTimes ["82: - -", "ROSA: - -"]
+    let busTimes = map (renderBusLine $ read @TimeOfDay (time ++ ":00"))
+                     $ interestingLines $ getLines lvg <> getLines hvs
+    runInky $ do img <- setup
+                 updateDisplay img busTimes 
 
  
